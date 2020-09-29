cbd6af07f573af307caf6d02e56d5087c554ab80
gokaybiz <gkymhmt@gmail.com>
2020-09-29 23:14:39 +0300

    No need indexdb

--- base/scripts/makeDbList.sh
+++ base/scripts/makeDbList.sh
@@ -2,14 +2,11 @@
 
 dbFasta=$(find /usr/share/rRNA_databases -name "*.fasta")
 dbLen=0
-for i in $dbFasta; do let dbLen+=1; done
+for i in $dbFasta; do dbLen+=1; done
 
 currDb=0
-echo -n "SORTMERNA_DB="
 for db in $dbFasta; do
-    let currDb+=1;
-    nam=$(basename $db);
-    echo -n "$db,/usr/share/rRNA_databases/index/${nam/.fasta/}";
-    [[ currDb -lt dbLen ]] && echo -n ":";
+    currDb=$((currDb+1));
+    echo -n "-ref $db ";
 done
 echo
